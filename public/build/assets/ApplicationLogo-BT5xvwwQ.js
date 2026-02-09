2398438bbde720dc0a2ce0b5a91cf58c97632a3b
ManboyFxx <ewerton147@gmail.com>
2026-02-08 22:58:50 -0300

    Fix: Include built assets (public/build) for production deploy

---
+++ public/build/assets/ApplicationLogo-BT5xvwwQ.js
@@ -0,0 +1,1 @@
+import{j as t}from"./vendor-react-BRbeZxQj.js";function a(o){return t.jsx("img",{src:"/images/logo-icon.png",alt:"ÓoDelivery",width:"100",height:"100",loading:"lazy",className:"w-auto h-full object-contain",style:{maxHeight:"100%",maxWidth:"100%"},...o})}export{a as A};
