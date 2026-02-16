3f3e0eeb789761a28d487a27279f4bd09d34483e
ManboyFxx <ewerton147@gmail.com>
2026-02-15 22:58:25 -0300

    chore: remove ooprint source code and installer from public/downloads

--- ooprint/electron/preload.cjs
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    print: (content, options) => ipcRenderer.invoke('print', content, options),
-    getPrinters: () => ipcRenderer.invoke('get-printers'),
-    isElectron: true
-});
